6d815da9c30f56e574a7342e025932ead78bd86b
kishan <kishan@innovix-labs.com>
2025-01-07 11:55:24 +0530

    removed console

--- src/database/services/attributeOption.services.ts
+++ src/database/services/attributeOption.services.ts
@@ -68,7 +68,7 @@ export const findAttributeByName = async (attributeName: string) => {
 export const findAttributeOptionById = async (id: string) => {
   try {
     const attributeDetails = await Attribute.findById(id);
-    console.log(attributeDetails, 'attributeDetails');
+
     return attributeDetails;
   } catch (err) {
     throw err;
