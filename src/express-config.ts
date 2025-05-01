0a031ee02be850bb15dcc6e2e163c182ddbeb987
kishan <kishan@innovix-labs.com>
2025-05-01 12:36:34 +0530

    fix size issue

--- src/express-config.ts
+++ src/express-config.ts
@@ -58,8 +58,8 @@ const configureExpress = async (app: Express) => {
   app.use(limiter);
 
   app.use(cors(corsOptions));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
   app.use('/uploads', express.static('uploads'));
   app.use('/api/v1', routes);
   app.use(errorHandler);
