53bf353c954ab8ddefe262c65f1b636eac4fa921
kishan <kishan@innovix-labs.com>
2025-06-11 17:45:25 +0530

    added entity and data source seed

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 import config from '../config';
-// import { seedUsers } from './user.seed';
-// import { seedOrganizations } from './organization.seed';
+import { seedUsers } from './user.seed';
+import { seedOrganizations } from './organization.seed';
 // import { seedCustomReports } from './customReport.seed';
 // import path from 'path';
 import { seedChart } from './widget_Type_chart.seed';
@@ -10,15 +10,19 @@ import { seedOperators } from './operators.seed';
 import { seedWidgetTheme } from './widgetTheme.seed';
 // import { seedDashboardWidget } from './dashboardWidget.seed';
 import { seedDashboard } from './dashboard.seed';
+import { seedEntities } from './entities.seed';
 
 const payload = {
-  superAdminUserId: new mongoose.Types.ObjectId('66b34cbbd40e24fca2e3e312'),
-  adminUserId: new mongoose.Types.ObjectId('64d229e76e4d3f1d2f9f3e8c'),
-  testUserId: new mongoose.Types.ObjectId('66b34cbbd40e24fca2e3e360'),
-  organizationId: new mongoose.Types.ObjectId('66de96d3548d06560e2931cb'),
-  superAdminWorkspaceId: new mongoose.Types.ObjectId('670fa939dd2e7e82ec55ac4d'),
-  adminWorkspaceId: new mongoose.Types.ObjectId('670fa947dd2e7e82ec55ac4e'),
-  userWorkspaceId: new mongoose.Types.ObjectId('66c6f88773caaef93d40807a'),
+  reportivixSuperAdminUserId: new mongoose.Types.ObjectId('66b34cbbd40e24fca2e3e312'),
+  reportivixAdminUserId: new mongoose.Types.ObjectId('64d229e76e4d3f1d2f9f3e8c'),
+  reportivixTestUserId: new mongoose.Types.ObjectId('66b34cbbd40e24fca2e3e360'),
+
+  sabicAdminUserId: new mongoose.Types.ObjectId('67caac8cc4f70d4fdbdb7299'),
+  mahuaAdminUserId: new mongoose.Types.ObjectId('67cb46f5fd7215371ed92173'),
+  //organization
+  reportivixOrganizationId: new mongoose.Types.ObjectId('66de96d3548d06560e2931cb'),
+  sabicOrganizationId: new mongoose.Types.ObjectId('67caac1ec4f70d4fdbdb7294'),
+
   pieChartId: new mongoose.Types.ObjectId('67e5011c966d261de673d1f1'),
   lineChartId: new mongoose.Types.ObjectId('67e68fd541db187651d5e6b8'),
   areaChartId: new mongoose.Types.ObjectId('67f3a9f7a3668434210f2e18'),
@@ -43,112 +47,13 @@ export async function seedDatabase() {
 
     // Seed individual collections
     console.info('\n====> Seeding users <====');
-    // await seedUsers(payload);
+    await seedUsers(payload);
+
+    console.info('\n====> Seeding Entities Widget <====');
+    await seedEntities(payload);
 
     console.info('\n====> Seeding organizations <====');
-    // await seedOrganizations(payload);
-
-    console.info('\n====> Seeding monthly ip <====');
-    // await seedCustomReports({
-    //   _id: '66b34cbbd40e24fca2e3e313',
-    //   reportName: 'monthlyip',
-    //   functionName: 'generateMonthlyIpReport',
-    //   dataSourceIds: [
-    //     {
-    //       code: 'portfolio',
-    //       dataSourceId: '67923590d31b8f4ea211147b',
-    //       fileDetails: [{ name: 'Complete Portfolio',isRequired:true }, { name: 'Complete Portfolio _SHPP' ,isRequired:false }],
-    //     },
-    //     {
-    //       code: 'disclosure',
-    //       dataSourceId: '6792332a753ceb4945e5b3b8',
-    //       fileDetails: [{ name: 'All Disclosures',,isRequired:true  }, { name: 'All Disclosures _SHPP' ,isRequired:false }],
-    //     },
-    //     { code: 'sabicip', dataSourceId: '67a3226221f5f8dd752ce644', fileDetails: [{ name: 'AnnuitiesDueList_SHPP' ,isRequired:false }] },
-    //     {
-    //       code: 'ctclinsab',
-    //       dataSourceId: '67a33c7f22cb8a927a85e427',
-    //       fileDetails: [{ name: 'AnnuitiesDueList_Linde',,isRequired:true  }],
-    //     },
-    //     {
-    //       code: 'annuities',
-    //       dataSourceId: '67a3429322cb8a927a85e4b9',
-    //       fileDetails: [{ name: 'AnnuitiesDueList_CPi' ,isRequired:true }],
-    //     },
-    //   ],
-    //   organizationId: payload.organizationId,
-    //   sampleFilePath: path.join('reports', 'sample', 'sample-monthly-ip-report.xlsx'),
-    // });
-    // await seedCustomReports({
-    //   _id: '66b34cbbd40e24fca2e3e314',
-    //   reportName: 'supplementalip',
-    //   functionName: 'generateSupplementalIpReport',
-    //   dataSourceIds: [
-    //     {
-    //       code: 'portfolio',
-    //       dataSourceId: '67923590d31b8f4ea211147b',
-    //       fileDetails: [{ name: 'Complete Portfolio' ,isRequired:true }, { name: 'Complete Portfolio _SHPP' ,isRequired:false}],
-    //     },
-    //     {
-    //       code: 'disclosure',
-    //       dataSourceId: '6792332a753ceb4945e5b3b8',
-    //       fileDetails: [{ name: 'All Disclosures',isRequired:true  }, { name: 'All Disclosures _SHPP',isRequired:false  }],
-    //     },
-    //     { code: 'sabicip', dataSourceId: '67a3226221f5f8dd752ce644', fileDetails: [{ name: 'AnnuitiesDueList_SHPP' ,isRequired:false}] },
-    //     {
-    //       code: 'ctclinsab',
-    //       dataSourceId: '67a33c7f22cb8a927a85e427',
-    //       fileDetails: [{ name: 'AnnuitiesDueList_Linde' ,isRequired:true }],
-    //     },
-    //     {
-    //       code: 'annuities',
-    //       dataSourceId: '67a3429322cb8a927a85e4b9',
-    //       fileDetails: [{ name: 'AnnuitiesDueList_CPi',isRequired:true  }],
-    //     },
-    //     {
-    //       code: 'sabiccontracts',
-    //       dataSourceId: '67c6d4eb9fb857c9aaf5319c',
-    //       fileDetails: [{ name: 'SABIC Contracts' ,isRequired:true }],
-    //     },
-    //     {
-    //       code: 'shppcontracts',
-    //       dataSourceId: '67c6d4fd9fb857c9aaf531b1',
-    //       fileDetails: [{ name: 'SHPP Contracts',isRequired:true  }],
-    //     },
-    //     {
-    //       code: 'ksacontracts',
-    //       dataSourceId: '67c6d50e9fb857c9aaf531c6',
-    //       fileDetails: [{ name: 'KSA Contracts' ,isRequired:true }],
-    //     },
-    //     {
-    //       code: 'attorneymapping',
-    //       dataSourceId: '67c6d5279fb857c9aaf531db',
-    //       fileDetails: [{ name: 'Required Mapping-Contracts', sheetName: 'Attorney Mapping',isRequired:true  }],
-    //     },
-    //     {
-    //       code: 'agreementtypemapping',
-    //       dataSourceId: '67c6d5499fb857c9aaf531f0',
-    //       fileDetails: [{ name: 'Required Mapping-Contracts', sheetName: 'Agreement Type Mapping' ,isRequired:true }],
-    //     },
-    //     {
-    //       code: 'ipanalystdashboard',
-    //       dataSourceId: '67c6e4ba44cef11611823e70',
-    //       fileDetails: [{ name: 'IP Analyst Dashboard' ,isRequired:true }],
-    //     },
-    //     {
-    //       code: 'shppaccolade',
-    //       dataSourceId: '67c6e4f144cef11611823e8a',
-    //       fileDetails: [{ name: 'SHPP Accolade',isRequired:true  }],
-    //     },
-    //     {
-    //       code: 'sabicaccolade',
-    //       dataSourceId: '67c6e50a44cef11611823e9e',
-    //       fileDetails: [{ name: 'SABIC Accolade',isRequired:true  }],
-    //     },
-    //   ],
-    //   organizationId: payload.organizationId,
-    //   sampleFilePath: '',
-    // });
+    await seedOrganizations(payload);
 
     console.info('\n====> Seeding Dashboard <====');
     await seedDashboard();
--- src/seeders/user.seed.ts
+++ src/seeders/user.seed.ts
@@ -2,23 +2,25 @@ import User from '../database/models/user';
 import { hashPassword } from '../utils/bcrypt.utils';
 
 export async function seedUsers(payload) {
-  const hashedSuperAdminPassword = await hashPassword('superadmin@1234');
-  const hashedAdminPassword = await hashPassword('admin@1234');
-  const hashedTestPassword = await hashPassword('test@1234');
+  const reportivixHashedSuperAdminPassword = await hashPassword('superadmin@1234');
+  const reportivixHashedAdminPassword = await hashPassword('admin@1234');
+  const reportivixHashedTestPassword = await hashPassword('test@1234');
 
   // Check if the Super Admin User already exists
-  const existingSuperAdminUser = await User.findById(payload.superAdminUserId);
+  const existingSuperAdminUser = await User.findById(payload.reportivixSuperAdminUserId);
 
   if (!existingSuperAdminUser) {
     const superAdminUser = new User({
-      _id: payload.superAdminUserId,
+      _id: payload.reportivixSuperAdminUserId,
       email: 'superadmin@reportivix.com',
-      password: hashedSuperAdminPassword,
+      password: reportivixHashedSuperAdminPassword,
       firstName: 'Super Admin',
       lastName: 'User',
       role: 'super admin',
       roleId: 1,
-      organizationId: payload.organizationId,
+      organizationId: payload.reportivixOrganizationId,
+      lastLogin: null,
+      status: 'active',
       createdAt: new Date('2024-08-07'),
       updatedAt: new Date('2024-08-07'),
     });
@@ -28,18 +30,20 @@ export async function seedUsers(payload) {
   }
 
   // Check if the Admin User already exists
-  const existingAdminUser = await User.findById(payload.adminUserId);
+  const existingAdminUser = await User.findById(payload.reportivixAdminUserId);
 
   if (!existingAdminUser) {
     const adminUser = new User({
-      _id: payload.adminUserId,
+      _id: payload.reportivixAdminUserId,
       email: 'admin@reportivix.com',
-      password: hashedAdminPassword,
+      password: reportivixHashedAdminPassword,
       firstName: 'Admin',
       lastName: 'User',
       role: 'admin',
       roleId: 2,
-      organizationId: payload.organizationId,
+      organizationId: payload.reportivixOrganizationId,
+      lastLogin: null,
+      status: 'active',
       createdAt: new Date('2024-08-08'),
       updatedAt: new Date('2024-08-08'),
     });
@@ -49,17 +53,20 @@ export async function seedUsers(payload) {
   }
 
   // Check if the Test User already exists
-  const existingTestUser = await User.findById(payload.testUserId);
+  const existingTestUser = await User.findById(payload.reportivixTestUserId);
 
   if (!existingTestUser) {
     const testUser = new User({
-      _id: payload.testUserId,
+      _id: payload.reportivixTestUserId,
       email: 'test@reportivix.com',
-      password: hashedTestPassword,
+      password: reportivixHashedTestPassword,
       firstName: 'Test',
       lastName: 'User',
       role: 'user',
-      organizationId: payload.organizationId,
+      roleId: 3,
+      organizationId: payload.reportivixOrganizationId,
+      lastLogin: null,
+      status: 'active',
       createdAt: new Date('2024-08-09'),
       updatedAt: new Date('2024-08-09'),
     });
@@ -68,22 +75,48 @@ export async function seedUsers(payload) {
     console.info('Test User created successfully.');
   }
 
-  // Set payload.organizationId for users without it
-  const organizationCount = await User.updateMany(
-    { organizationId: { $exists: false } },
-    { $set: { organizationId: payload.organizationId } },
-  );
-  console.info(`Updated ${organizationCount.modifiedCount} users with organizationIds.`);
+  //sabic user
+  // Check if the Admin User already exists
+  const existingSabicAdminUser = await User.findById(payload.sabicAdminUserId);
+  const sabicHashedAdminPassword = await hashPassword('Sabic@1234');
+  if (!existingSabicAdminUser) {
+    const sabicAdminUser = new User({
+      _id: payload.sabicAdminUserId,
+      email: 'cs.sabic@innovix-labs.com',
+      password: sabicHashedAdminPassword,
+      firstName: 'Sabic',
+      lastName: 'Admin',
+      role: 'admin',
+      roleId: 2,
+      organizationId: payload.sabicOrganizationId,
+      lastLogin: null,
+      status: 'active',
+      createdAt: new Date('2024-08-08'),
+      updatedAt: new Date('2024-08-08'),
+    });
 
-  const updateLastLogin = await User.updateMany({ lastLogin: { $exists: false } }, { $set: { lastLogin: null } });
-  console.info(`Updated ${updateLastLogin.modifiedCount} users with lastLogin.`);
+    await sabicAdminUser.save();
+    console.info('Sabic Admin created successfully.');
+  }
+  const existingSabicAdminMahuaUser = await User.findById(payload.mahuaAdminUserId);
+  const sabicHashedAdminMahuaPassword = await hashPassword('Sabic@1234');
+  if (!existingSabicAdminMahuaUser) {
+    const sabicMahuaAdminUser = new User({
+      _id: payload.mahuaAdminUserId,
+      email: 'mahua.dutta@sabic.com',
+      password: sabicHashedAdminMahuaPassword,
+      firstName: 'Mahua',
+      lastName: 'Dutta',
+      role: 'admin',
+      roleId: 2,
+      organizationId: payload.sabicOrganizationId,
+      lastLogin: null,
+      status: 'active',
+      createdAt: new Date('2024-08-08'),
+      updatedAt: new Date('2024-08-08'),
+    });
 
-  const adminRole = await User.updateMany({ role: 'admin', roleId: 1 }, { $set: { roleId: 2 } });
-  console.info(`Updated ${adminRole.modifiedCount} users with admin roleIds`);
-
-  const userRole = await User.updateMany({ role: 'user', roleId: 2 }, { $set: { roleId: 3 } });
-  console.info(`Updated ${userRole.modifiedCount} users with user roleIds`);
-
-  const updateStatus = await User.updateMany({ status: { $exists: false } }, { $set: { status: 'active' } });
-  console.info(`Updated ${updateStatus.modifiedCount} users with status.`);
+    await sabicMahuaAdminUser.save();
+    console.info('Sabic Mahua Admin created successfully.');
+  }
 }
