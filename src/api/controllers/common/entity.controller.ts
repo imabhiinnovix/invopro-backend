a8bdb54255be419f296d78625ffe9c07b1c789d9
kishan <kishan@innovix-labs.com>
2025-07-29 10:11:27 +0530

    added org id in enetity list

--- src/api/controllers/common/entity.controller.ts
+++ src/api/controllers/common/entity.controller.ts
@@ -57,8 +57,8 @@ export const listEntity = async (req: Request, res: Response, next: NextFunction
     const { search, paginate = 'false' } = req.query;
     const page = parseInt(req.query.page as string, 10) || 1;
     const limit = parseInt(req.query.limit as string, 10) || 10;
-
-    const query: any = {};
+    const { organizationId } = req.user;
+    const query: any = { organizationId };
     if (search) query.name = { $regex: search, $options: 'i' };
 
     let result: any = {};
