19972a95219a000efd5648a03930b3406c18e541
kishan <kishan@innovix-labs.com>
2025-08-01 15:28:07 +0530

    api added to resolve

--- src/api/routes/common/dataImportError.routes.ts
+++ src/api/routes/common/dataImportError.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import { authenticateToken } from '../../../middlewares/authenticate.middleware';
-import { listDataSourceVersionErrorBasedOnDataSourceVersionId } from '../../controllers/common/dataImportError.controller';
+import {
+  listDataSourceVersionErrorBasedOnDataSourceVersionId,
+  resolveDataImportError,
+} from '../../controllers/common/dataImportError.controller';
 import { permissionMiddleware } from '../../../middlewares/permission.middleware';
 
 const router = Router();
 
 router.get('/list', authenticateToken, permissionMiddleware(), listDataSourceVersionErrorBasedOnDataSourceVersionId);
+router.post(
+  '/resolve',
+  authenticateToken,
+  //   permissionMiddleware(),
+  resolveDataImportError
+);
 
 export default router;
