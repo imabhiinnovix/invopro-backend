8d54a84b6918d73df7b93992effa9eee45d46b39
kishan <kishan@innovix-labs.com>
2025-08-29 11:32:52 +0530

    added datasource id in response

--- src/api/controllers/common/dataImportError.controller.ts
+++ src/api/controllers/common/dataImportError.controller.ts
@@ -235,7 +235,7 @@ export const resolveDataImportError = async (req: Request, res: Response, next: 
     } else {
       return res.status(400).json({ success: false, message: 'Not valid action.' });
     }
-    return res.status(200).json({ success: true, message: 'Action Applied.' });
+    return res.status(200).json({ success: true, message: 'Action Applied.', data: { dataSourceId } });
   } catch (err) {
     next(err);
   }
