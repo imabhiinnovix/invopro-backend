5e4ffa612d4aed0a764d912b34d5714fe279124e
Abhishek Singh <abhishek@innovix-labs.com>
2025-12-15 18:25:07 +0530

    crud filter sorting changes

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -848,28 +848,28 @@ export async function seedDatabase() {
         resourceType: 'Data Source',
         code: 'case_list',
         organizationId: payload.reportivixOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'Action Due',
         dataSourceId: entityDataSourceMapReportivix.action_due.dataSourceId,
         code: 'action_due',
         organizationId: payload.reportivixOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'IP Counsel',
         dataSourceId: entityDataSourceMapReportivix.ip_counsel.dataSourceId,
         code: 'ip_counsel',
         organizationId: payload.reportivixOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'Formality Officers',
         dataSourceId: entityDataSourceMapReportivix.formality_officers.dataSourceId,
         code: 'formality_officers',
         organizationId: payload.reportivixOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'Case List',
@@ -877,28 +877,28 @@ export async function seedDatabase() {
         resourceType: 'Data Source',
         code: 'case_list',
         organizationId: payload.sabicOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'Action Due',
         dataSourceId: entityDataSourceMapSabic.action_due.dataSourceId,
         code: 'action_due',
         organizationId: payload.sabicOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'IP Counsel',
         dataSourceId: entityDataSourceMapSabic.ip_counsel.dataSourceId,
         code: 'ip_counsel',
         organizationId: payload.sabicOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
       {
         name: 'Formality Officers',
         dataSourceId: entityDataSourceMapSabic.formality_officers.dataSourceId,
         code: 'formality_officers',
         organizationId: payload.sabicOrganizationId,
-        module: "Notification"
+        module: "Notifications"
       },
     ]);
 
